616eb38e4fddb81dc0dce11cf4a090ffd220568b
zhangjinhui <zhangjinhui@itcast.cn>
2019-07-05 10:41:35 +0800

    向本地仓库添加UserTest

---
+++ git-test-project/src/com/itheima/git/UserTest.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("张晋珲");
+        user.setGender("男");
+        user.setAge(20);
+        System.out.println(user);
+    }
+}
